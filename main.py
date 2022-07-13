f0038d776ed7c749c18458b04663642f2facf94a
KondratevYaroslav <iamawesome@mail.ru>
2022-07-14 01:11:03 +0300

    change count

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def start():
     
 В этой игре ты столкнешься с невероятными математическми операциями. Будь осторожен ибо математика коварна и опасна!
 Но ты всегда можешь остановиться введя слово 'СТОП'
-Тебе будет задано 10 примеров. За каждую ошибку ты теряешь одну жизнь. Постарайся дойти до конца!
+У тебя есть три жизни. За каждую ошибку ты теряешь одну жизнь. Постарайся продержаться как можно дальше!
 """)
     print()
 
@@ -20,7 +20,8 @@ def start():
 def stop():
     print(f"""Игра закончена... 
 Всего было примеров - {count}
-Правильных ответов - {count-miss}""")
+Правильных ответов - {count-miss}
+Процент побед - {((count - miss) / count) * 100}%""")
 
 
 def checkresult(result, z):
@@ -35,11 +36,12 @@ def checkresult(result, z):
 
 
 def funcsum():
+    global count
     result = ""
     z = random.randint(minDiapazon, maxDiapazon)
     x = random.randint(minDiapazon, z)
     y = z - x
-
+    count += 1
     if random.randint(0, 1):
         while not result.isdigit():
             result = input(f"{x} + {y} = ")
@@ -54,10 +56,12 @@ def funcsum():
 
 
 def funcSubtraction():
+    global count
     result = ""
     x = random.randint(minDiapazon + 15, maxDiapazon - 15)
     y = random.randint(minDiapazon, maxDiapazon)
     z = abs(x - y)
+    count += 1
     while not result.isdigit():
         result = input(f"{max(x,y)} - {min(x,y)} = ")
         if not result.isdigit():
@@ -65,12 +69,13 @@ def funcSubtraction():
     checkresult(int(result), z)
 
 
-
 def funcMultiplication():
+    global count
     result = ""
     x = random.randint(minDiapazon, maxDiapazon) // 5 + 1
     y = random.randint(minDiapazon, maxDiapazon) // x
     z = x * y
+    count += 1
     if random.randint(0, 1):
         while not result.isdigit():
             result = input(f"{x} * {y} = ")
@@ -82,24 +87,27 @@ def funcMultiplication():
             if not result.isdigit():
                 print("Введите число!")
     checkresult(int(result), z)
+
+
 def funcDivision():
+    global count
     result = ""
     x = random.randint(minDiapazon, maxDiapazon) // 10 + 1
     y = random.randint(minDiapazon, maxDiapazon) // x
     x = x * y
     z = x // y
+    count += 1
     while not result.isdigit():
         result = input(f"{x} / {y} = ")
         if not result.isdigit():
             print("Введите число!")
     checkresult(int(result), z)
 
+
 def main():
     global count
-
     start()
     while lives:
-        count += 1
         func = random.randint(0, 4)
         if func == 0:
             funcsum()
